29f8e1d02e9f9e1a5581d78c0e781ecd649ef3b0
jwk70 <jwk70@DESKTOP-75TQCTV>
2022-07-05 16:36:46 +0900

    초기 커밋.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DpsCamera
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inqureButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inqureButton
+            // 
+            this.inqureButton.Location = new System.Drawing.Point(649, 519);
+            this.inqureButton.Name = "inqureButton";
+            this.inqureButton.Size = new System.Drawing.Size(232, 102);
+            this.inqureButton.TabIndex = 0;
+            this.inqureButton.Text = "조회";
+            this.inqureButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1239, 727);
+            this.Controls.Add(this.inqureButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inqureButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DpsCamera {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+
+            this.inqureButton.Click += inquireButtonClicked;
+        }
+
+        public void inquireButtonClicked(object sender, EventArgs e) {
+            InquireForm inquireForm = new InquireForm();
+            inquireForm.Show();
+        }
+    }
+}
