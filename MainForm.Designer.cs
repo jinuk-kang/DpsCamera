b25e3efe7e05d9b8aae968d5d355efc64652ec63
Jin <jwk70915@gmail.com>
2022-07-06 14:58:43 +0900

    Add camera connection code

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.inqureButton = new System.Windows.Forms.Button();
+            this.inquireButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -37,26 +37,31 @@
             this.label6 = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.connectCameraButton = new System.Windows.Forms.Button();
+            this.disconnectCameraButton = new System.Windows.Forms.Button();
+            this.endButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // inqureButton
+            // inquireButton
             // 
-            this.inqureButton.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.inqureButton.Location = new System.Drawing.Point(351, 559);
-            this.inqureButton.Name = "inqureButton";
-            this.inqureButton.Size = new System.Drawing.Size(200, 89);
-            this.inqureButton.TabIndex = 0;
-            this.inqureButton.Text = "조회";
-            this.inqureButton.UseVisualStyleBackColor = true;
+            this.inquireButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.inquireButton.Location = new System.Drawing.Point(271, 699);
+            this.inquireButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.inquireButton.Name = "inquireButton";
+            this.inquireButton.Size = new System.Drawing.Size(229, 111);
+            this.inquireButton.TabIndex = 0;
+            this.inquireButton.Text = "조회";
+            this.inquireButton.UseVisualStyleBackColor = true;
+            this.inquireButton.Click += new System.EventHandler(this.inquireButtonClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("나눔고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(632, 92);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(722, 290);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.Size = new System.Drawing.Size(56, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "날짜";
             // 
@@ -64,21 +69,21 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Info;
-            this.label2.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(762, 80);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(871, 275);
             this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(4);
-            this.label2.Size = new System.Drawing.Size(168, 36);
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(191, 46);
             this.label2.TabIndex = 2;
             this.label2.Text = "2022-07-05";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("나눔고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(632, 163);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(722, 379);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 24);
+            this.label3.Size = new System.Drawing.Size(77, 31);
             this.label3.TabIndex = 3;
             this.label3.Text = "바코드";
             // 
@@ -86,21 +91,21 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.Info;
-            this.label4.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(762, 151);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(871, 364);
             this.label4.Name = "label4";
-            this.label4.Padding = new System.Windows.Forms.Padding(4);
-            this.label4.Size = new System.Drawing.Size(244, 36);
+            this.label4.Padding = new System.Windows.Forms.Padding(5);
+            this.label4.Size = new System.Drawing.Size(277, 46);
             this.label4.TabIndex = 4;
             this.label4.Text = "12345678912345";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("나눔고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(632, 242);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(722, 477);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 24);
+            this.label5.Size = new System.Drawing.Size(98, 31);
             this.label5.TabIndex = 5;
             this.label5.Text = "작업수량";
             // 
@@ -108,38 +113,74 @@
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.SystemColors.Info;
-            this.label6.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label6.Location = new System.Drawing.Point(762, 230);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(871, 463);
             this.label6.Name = "label6";
-            this.label6.Padding = new System.Windows.Forms.Padding(4);
-            this.label6.Size = new System.Drawing.Size(68, 36);
+            this.label6.Padding = new System.Windows.Forms.Padding(5);
+            this.label6.Size = new System.Drawing.Size(79, 46);
             this.label6.TabIndex = 6;
             this.label6.Text = "150";
             // 
             // startButton
             // 
-            this.startButton.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.startButton.Location = new System.Drawing.Point(636, 559);
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.startButton.Location = new System.Drawing.Point(570, 699);
+            this.startButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(200, 89);
+            this.startButton.Size = new System.Drawing.Size(229, 111);
             this.startButton.TabIndex = 7;
             this.startButton.Text = "작업 시작";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButtonClick);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.pictureBox1.Location = new System.Drawing.Point(115, 66);
+            this.pictureBox1.Location = new System.Drawing.Point(131, 82);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(436, 405);
+            this.pictureBox1.Size = new System.Drawing.Size(498, 506);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
+            // connectCameraButton
+            // 
+            this.connectCameraButton.Location = new System.Drawing.Point(727, 123);
+            this.connectCameraButton.Name = "connectCameraButton";
+            this.connectCameraButton.Size = new System.Drawing.Size(158, 76);
+            this.connectCameraButton.TabIndex = 9;
+            this.connectCameraButton.Text = "카메라 연결";
+            this.connectCameraButton.UseVisualStyleBackColor = true;
+            this.connectCameraButton.Click += new System.EventHandler(this.connectCameraButtonClick);
+            // 
+            // disconnectCameraButton
+            // 
+            this.disconnectCameraButton.Location = new System.Drawing.Point(935, 123);
+            this.disconnectCameraButton.Name = "disconnectCameraButton";
+            this.disconnectCameraButton.Size = new System.Drawing.Size(153, 76);
+            this.disconnectCameraButton.TabIndex = 10;
+            this.disconnectCameraButton.Text = "연결 해제";
+            this.disconnectCameraButton.UseVisualStyleBackColor = true;
+            this.disconnectCameraButton.Click += new System.EventHandler(this.disconnectCameraButtonClick);
+            // 
+            // endButton
+            // 
+            this.endButton.Location = new System.Drawing.Point(890, 699);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(237, 111);
+            this.endButton.TabIndex = 11;
+            this.endButton.Text = "작업 종료";
+            this.endButton.UseVisualStyleBackColor = true;
+            this.endButton.Click += new System.EventHandler(this.endButtonClick);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1239, 727);
+            this.ClientSize = new System.Drawing.Size(1416, 909);
+            this.Controls.Add(this.endButton);
+            this.Controls.Add(this.disconnectCameraButton);
+            this.Controls.Add(this.connectCameraButton);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.label6);
@@ -148,8 +189,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.inqureButton);
+            this.Controls.Add(this.inquireButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -161,7 +203,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button inqureButton;
+        private System.Windows.Forms.Button inquireButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -170,6 +212,9 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button connectCameraButton;
+        private System.Windows.Forms.Button disconnectCameraButton;
+        private System.Windows.Forms.Button endButton;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -5,34 +5,156 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using MvCamCtrl.NET;
+using System.Runtime.InteropServices;
 
 namespace DpsCamera {
     public partial class MainForm : Form {
         bool isWorking = false;
+        bool m_bGrabbing = false;
+        Thread m_hReceiveThread = null;
+        IntPtr m_BufForDriver = IntPtr.Zero;
+
+        List<string> cameraList = new List<string>();
+        MyCamera.MV_CC_DEVICE_INFO_LIST m_stDeviceList = new MyCamera.MV_CC_DEVICE_INFO_LIST();
+        private MyCamera m_MyCamera = new MyCamera();
 
         public MainForm() {
             InitializeComponent();
 
-            this.inqureButton.Click += inquireButtonClicked;
-            this.startButton.Click += startButtonClicked;
+            acquireCameraList();
+            setWorkStatus(false);
         }
 
-        public void inquireButtonClicked(object sender, EventArgs e) {
+        private void acquireCameraList() {
+            // Create Device List
+            System.GC.Collect();
+            cameraList.Clear();
+            m_stDeviceList.nDeviceNum = 0;
+            int nRet = MyCamera.MV_CC_EnumDevices_NET(MyCamera.MV_GIGE_DEVICE | MyCamera.MV_USB_DEVICE, ref m_stDeviceList);
+            if (0 != nRet) {
+                MessageBox.Show("Enumerate devices fail!");
+                return;
+            }
+
+            // Display device name in the form list
+            for (int i = 0; i < m_stDeviceList.nDeviceNum; i++) {
+                MyCamera.MV_CC_DEVICE_INFO device = (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(m_stDeviceList.pDeviceInfo[i], typeof(MyCamera.MV_CC_DEVICE_INFO));
+                if (device.nTLayerType == MyCamera.MV_GIGE_DEVICE) {
+                    MyCamera.MV_GIGE_DEVICE_INFO gigeInfo = (MyCamera.MV_GIGE_DEVICE_INFO)MyCamera.ByteToStruct(device.SpecialInfo.stGigEInfo, typeof(MyCamera.MV_GIGE_DEVICE_INFO));
+
+                    if (gigeInfo.chUserDefinedName != "") {
+                        cameraList.Add("GEV: " + gigeInfo.chUserDefinedName + " (" + gigeInfo.chSerialNumber + ")");
+                    } else {
+                        cameraList.Add("GEV: " + gigeInfo.chManufacturerName + " " + gigeInfo.chModelName + " (" + gigeInfo.chSerialNumber + ")");
+                    }
+                } else if (device.nTLayerType == MyCamera.MV_USB_DEVICE) {
+                    MyCamera.MV_USB3_DEVICE_INFO usbInfo = (MyCamera.MV_USB3_DEVICE_INFO)MyCamera.ByteToStruct(device.SpecialInfo.stUsb3VInfo, typeof(MyCamera.MV_USB3_DEVICE_INFO));
+                    if (usbInfo.chUserDefinedName != "") {
+                        cameraList.Add("U3V: " + usbInfo.chUserDefinedName + " (" + usbInfo.chSerialNumber + ")");
+                    } else {
+                        cameraList.Add("U3V: " + usbInfo.chManufacturerName + " " + usbInfo.chModelName + " (" + usbInfo.chSerialNumber + ")");
+                    }
+                }
+            }
+
+            if(cameraList.Count == 0) {
+                MessageBox.Show("CameraList is empty");
+            } else {
+                MessageBox.Show(cameraList[0]);
+            }
+        }
+
+        private void connectCameraButtonClick(object sender, EventArgs e) {
+            if (m_stDeviceList.nDeviceNum == 0 || cameraList.Count == 0) {
+                MessageBox.Show("No device, please select");
+                return;
+            }
+
+            // Get selected device information
+            MyCamera.MV_CC_DEVICE_INFO device = (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(m_stDeviceList.pDeviceInfo[0], typeof(MyCamera.MV_CC_DEVICE_INFO));
+
+            // Open device
+            if (null == m_MyCamera) {
+                m_MyCamera = new MyCamera();
+                if (null == m_MyCamera) {
+                    return;
+                }
+            }
+
+            int nRet = m_MyCamera.MV_CC_CreateDevice_NET(ref device);
+            if (MyCamera.MV_OK != nRet) {
+                return;
+            }
+
+            nRet = m_MyCamera.MV_CC_OpenDevice_NET();
+            if (MyCamera.MV_OK != nRet) {
+                m_MyCamera.MV_CC_DestroyDevice_NET();
+                MessageBox.Show("Device open fail!\n" + nRet);
+                return;
+            }
+
+            // Detection network optimal package size(It only works for the GigE camera)
+            if (device.nTLayerType == MyCamera.MV_GIGE_DEVICE) {
+                int nPacketSize = m_MyCamera.MV_CC_GetOptimalPacketSize_NET();
+                if (nPacketSize > 0) {
+                    nRet = m_MyCamera.MV_CC_SetIntValue_NET("GevSCPSPacketSize", (uint)nPacketSize);
+                    if (nRet != MyCamera.MV_OK) {
+                        MessageBox.Show("Set Packet Size failed!\n" + nRet);
+                    }
+                } else {
+                    MessageBox.Show("Get Packet Size failed!\n" + nPacketSize);
+                }
+            }
+
+            // Set Continues Aquisition Mode
+            m_MyCamera.MV_CC_SetEnumValue_NET("AcquisitionMode", (uint)MyCamera.MV_CAM_ACQUISITION_MODE.MV_ACQ_MODE_CONTINUOUS);
+            m_MyCamera.MV_CC_SetEnumValue_NET("TriggerMode", (uint)MyCamera.MV_CAM_TRIGGER_MODE.MV_TRIGGER_MODE_OFF);
+
+            //bnGetParam_Click(null, null);// Get parameters
+
+            // Control operation
+            connectCameraButton.Enabled = false;
+            disconnectCameraButton.Enabled = true;
+        }
+        private void disconnectCameraButtonClick(object sender, EventArgs e) {
+            if (m_bGrabbing == true) {
+                m_bGrabbing = false;
+                m_hReceiveThread.Join();
+            }
+
+            if (m_BufForDriver != IntPtr.Zero) {
+                Marshal.Release(m_BufForDriver);
+            }
+
+            // Close Device
+            m_MyCamera.MV_CC_CloseDevice_NET();
+            m_MyCamera.MV_CC_DestroyDevice_NET();
+
+            connectCameraButton.Enabled = true;
+            disconnectCameraButton.Enabled = false;
+        }
+
+        private void inquireButtonClick(object sender, EventArgs e) {
             InquireForm inquireForm = new InquireForm();
             inquireForm.Show();
         }
 
-        public void startButtonClicked(object sedner, EventArgs e) {
-            if (isWorking) {
-                this.startButton.Text = "작업 시작";
-            } else {
-                this.startButton.Text = "작업 종료";
-            }
+        private void startButtonClick(object sender, EventArgs e) {
+            setWorkStatus(true);
+        }
+        private void endButtonClick(object sender, EventArgs e) {
+            setWorkStatus(false);
+        }
 
-            isWorking = !isWorking;
+        private void setWorkStatus(bool isWorking) {
+            this.isWorking = isWorking;
+            startButton.Enabled = !isWorking;
+            endButton.Enabled = isWorking;
+            connectCameraButton.Enabled = isWorking;
+            disconnectCameraButton.Enabled = false;
         }
     }
 }
